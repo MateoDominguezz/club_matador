0f6d777ae4e702a4b4a66037191082bfb6255f53
Mateo Dominguez <mateodominguez83@yahoo.com>
2025-06-07 12:36:30 -0300

    Ignorar config.php

--- matador/sources/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Definimos los valores de la base de datos
-//Se pone en git ignore
-define("HOST","localhost");
-define("USER","root");
-define("PASSWORD","");
-define("BASE","club_matador");
